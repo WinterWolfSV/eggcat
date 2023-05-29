4a82d89f03e51575a42686ddcd9c1fcf695147b0
WinterWolfSV <what112222@gmail.com>
2023-05-29 18:08:17 +0200

    Updated to 1.20-pre.7

--- src/main/java/net/winterwolfsv/eggcat/item/CustomItems.java
+++ src/main/java/net/winterwolfsv/eggcat/item/CustomItems.java
@@ -1,6 +1,5 @@
 package net.winterwolfsv.eggcat.item;
 
-import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.BlockItem;
@@ -14,19 +13,10 @@ import net.winterwolfsv.eggcat.Eggcat;
 import net.winterwolfsv.eggcat.block.CustomBlocks;
 
 public class CustomItems {
-    public static final Item EGGCAT = registerItem(new FabricItemSettings());
-
-
-    public static void registerItems() {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> entries.addAfter(Items.DRAGON_EGG, EGGCAT));
-    }
-
-    private static Item registerItem(Item.Settings settings) {
-        return Registry.register(Registries.ITEM, new Identifier(Eggcat.MODID, "eggcat"), new BlockItem(CustomBlocks.EGGCAT, settings));
-    }
-
+    public static final Item EGGCAT = Registry.register(Registries.ITEM, new Identifier(Eggcat.MODID, "eggcat"), new BlockItem(CustomBlocks.EGGCAT, new FabricItemSettings()));
+    
     public static void registerModItems() {
-        registerItems();
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> entries.addAfter(Items.DRAGON_EGG, EGGCAT));
     }
 
 }
